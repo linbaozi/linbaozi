1e73ce64f84993eed79939c35a5805faa63de1c8
linbaozi <1023019662@qq.com>
2015-11-21 22:49:12 +0800

    the second

---
+++ dh.cpp
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "windows.h"
+#include "conio.h"
+int main()
+{
+	int n;
+	n=1000;
+	printf("           __     __    \n");
+	printf("          I  I   I  I   \n");
+	printf("        (   O  U  O   ) \n");
+	printf("                        \n");
+	printf("          I  U   U  I   \n");
+	printf("          I         I   \n");
+	printf("             U   U  ==  \n");
+	printf("                        \n");
+    printf("       SO CUTE          \n"); 
+
+	Sleep(n);
+		system("color 28");
+    Sleep(n);
+	    system("color 75");
+	Sleep(n);
+		system("color 0F");
+     getch;
+
+    return 0;
+}
